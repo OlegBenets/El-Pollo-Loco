984694c9b8155f9e95492784d799498d5320388a
Oleg Benets <oleg.benets@web.de>
2024-06-04 16:19:31 +0200

    added models folder

--- js/game.js
+++ js/game.js
@@ -1,9 +1,10 @@
 let canvas;
 let ctx;
-let character = new Image();
+let character = new MovableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../img/2_character_pepe/2_walk/W-21.png';
+
+    console.log("My Character is", character);
 }
