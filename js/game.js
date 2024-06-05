9505dad410a11847a37de23d5146bc8277c6d309
Oleg Benets <oleg.benets@web.de>
2024-06-05 18:47:46 +0200

    implemented walk animation and keyboard class

--- js/game.js
+++ js/game.js
@@ -4,12 +4,38 @@ let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 
 
     console.log("My Character is", world.character);
 }
 
-window.addEventListener("keypress", (e) => {
-   console.log(e);
+window.addEventListener("keydown", (e) => {
+    if (e.keyCode === 37 || e.keyCode === 65) {
+        keyboard.LEFT = true;
+    } else if (e.keyCode === 39 || e.keyCode === 68) {
+        keyboard.RIGHT = true;
+    } else if (e.keyCode === 38 || e.keyCode === 87) {
+        keyboard.UP = true;
+    } else if (e.keyCode === 40 || e.keyCode === 83) {
+        keyboard.DOWN = true;
+    } else if (e.keyCode === 32) {
+        keyboard.SPACE = true;
+    }
+    console.log(`Keydown: ${e.keyCode}`, keyboard);
+});
+
+window.addEventListener("keyup", (e) => {
+    if (e.keyCode === 37 || e.keyCode === 65) {
+        keyboard.LEFT = false;
+    } else if (e.keyCode === 39 || e.keyCode === 68) {
+        keyboard.RIGHT = false;
+    } else if (e.keyCode === 38 || e.keyCode === 87) {
+        keyboard.UP = false;
+    } else if (e.keyCode === 40 || e.keyCode === 83) {
+        keyboard.DOWN = false;
+    } else if (e.keyCode === 32) {
+        keyboard.SPACE = false;
+    }
+    console.log(`Keyup: ${e.keyCode}`, keyboard);
 });
