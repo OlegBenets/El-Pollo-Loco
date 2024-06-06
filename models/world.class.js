23d72185cfb3b9005ac2c65611f3f2372373c7e9
Oleg Benets <oleg.benets@web.de>
2024-06-06 17:04:02 +0200

    implemented walk animation for character

--- models/world.class.js
+++ models/world.class.js
@@ -7,12 +7,11 @@ class World {
     new BackgroundObject("/img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObject("/img/5_background/layers/2_second_layer/1.png", 0),
     new BackgroundObject("/img/5_background/layers/1_first_layer/1.png", 0),
-    new BackgroundObject("/img/5_background/layers/1_first_layer/2.png", 0), 
+    new BackgroundObject("/img/5_background/layers/1_first_layer/2.png", 0),
   ];
   canvas;
   ctx;
   keyboard;
-
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -22,11 +21,9 @@ class World {
     this.setWorld();
   }
 
-
   setWorld() {
     this.character.world = this;
   }
-
 
   draw() {
     this.ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -50,6 +47,16 @@ class World {
   }
 
   addToMap(mo) {
+    if (mo.otherDirection) {
+      this.ctx.save();
+      this.ctx.translate(mo.width, 0);
+      this.ctx.scale(-1, 1);
+      mo.x = mo.x * -1;
+    }
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    if (mo.otherDirection) {
+      mo.x = mo.x * -1;
+      this.ctx.restore();
+    }
   }
 }
