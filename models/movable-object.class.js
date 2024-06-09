1e745922bba237bef0551016a5c55a77d3e7cd1f
Oleg Benets <oleg.benets@web.de>
2024-06-09 16:45:30 +0200

    added statusbar

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-  x = 120;
-  y = 280;
-  img;
-  height = 150;
-  width = 100;
-  imageCache = {};
-  currentImage = 0;
+class MovableObject extends DrawableObject {
   speed = 0.15;
   otherDirection = false;
   speedY = 0;
@@ -26,59 +19,33 @@ class MovableObject {
     return this.y < 180;
   }
 
-  loadImage(path) {
-    this.img = new Image(); // this.img = document.getElementById('image') <img id="image" src>
-    this.img.src = path;
+
+  isColliding(mo) {
+    return (
+      this.x + this.width > mo.x &&
+      this.y + this.height > mo.y &&
+      this.x < mo.x &&
+      this.y < mo.y + mo.height
+    );
   }
 
-  draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-  }
-
-  drawFrame(ctx) {
-    if (this instanceof Character || this instanceof Chicken) {
-      ctx.beginPath();
-      ctx.lineWidth = "5";
-      ctx.strokeStyle = "blue";
-      ctx.rect(this.x, this.y, this.width, this.height);
-      ctx.stroke();
+  hit() {
+    this.energy -= 5;
+    if (this.energy < 0) {
+      this.energy = 0;
+    } else {
+      this.lastHit = new Date().getTime();
     }
   }
 
-  isColliding(mo) {
-    return  this.x + this.width > mo.x &&
-    this.y + this.height > mo.y &&
-    this.x < mo.x &&
-    this.y < mo.y + mo.height;
-}
-
-
-hit() {
-  this.energy -= 5;
-  if(this.energy < 0) {
-    this.energy = 0;
-  } else {
-    this.lastHit = new Date().getTime();
+  isHurt() {
+    let timepassed = new Date().getTime() - this.lastHit;
+    timepassed = timepassed / 1000;
+    return timepassed < 1;
   }
-}
 
-isHurt() {
-  let timepassed = new Date().getTime() - this.lastHit;
-  timepassed = timepassed / 1000;
-  return timepassed < 1;
-}
-
-isDead() {
-  return this.energy == 0;
-}
-
-
-  loadImages(arr) {
-    arr.forEach((path) => {
-      let img = new Image();
-      img.src = path;
-      this.imageCache[path] = img;
-    });
+  isDead() {
+    return this.energy == 0;
   }
 
   playAnimation(images) {
