8b47fe97a38a3cbe4b6917b32fb7494f23e7ea47
Oleg Benets <oleg.benets@web.de>
2024-06-09 16:00:18 +0200

    added hurt and dead functions

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -10,6 +10,8 @@ class MovableObject {
   otherDirection = false;
   speedY = 0;
   acceleration = 2.5;
+  energy = 100;
+  lastHit = 0;
 
   applyGravity() {
     setInterval(() => {
@@ -29,19 +31,46 @@ class MovableObject {
     this.img.src = path;
   }
 
-
   draw(ctx) {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }
 
-
   drawFrame(ctx) {
-    ctx.beginPath();
-    ctx.lineWidth = "5";
-    ctx.strokeStyle = "blue";
-    ctx.rect(this.x, this.y, this.width, this.height);
-    ctx.stroke();
+    if (this instanceof Character || this instanceof Chicken) {
+      ctx.beginPath();
+      ctx.lineWidth = "5";
+      ctx.strokeStyle = "blue";
+      ctx.rect(this.x, this.y, this.width, this.height);
+      ctx.stroke();
+    }
   }
+
+  isColliding(mo) {
+    return  this.x + this.width > mo.x &&
+    this.y + this.height > mo.y &&
+    this.x < mo.x &&
+    this.y < mo.y + mo.height;
+}
+
+
+hit() {
+  this.energy -= 5;
+  if(this.energy < 0) {
+    this.energy = 0;
+  } else {
+    this.lastHit = new Date().getTime();
+  }
+}
+
+isHurt() {
+  let timepassed = new Date().getTime() - this.lastHit;
+  timepassed = timepassed / 1000;
+  return timepassed < 1;
+}
+
+isDead() {
+  return this.energy == 0;
+}
 
 
   loadImages(arr) {
