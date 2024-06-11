0f58f08907a77e8db70ddfa7b0d9b4f3eee38d5f
Oleg Benets <oleg.benets@web.de>
2024-06-11 19:32:05 +0200

    added sounds and animations

--- models/chicken.small.class.js
+++ models/chicken.small.class.js
@@ -14,7 +14,7 @@ class SmallChicken extends MovableObject {
         super().loadImage('/img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
 
-        this.x = 200 + Math.random() * 3100;
+        this.x = 500 + Math.random() * 3100;
         this.speed = 0.15 + Math.random() * 0.5;
         this.animate();
     }
